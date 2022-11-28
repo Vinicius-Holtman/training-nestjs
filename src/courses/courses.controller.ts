2bf5e5bc4d083a3c150c96f3b3ea374390b8e372
Vinicius <viniholtman28@gmail.com>
2022-11-28 08:59:19 -0300

    Create methods in service courses

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { CoursesService } from './courses.service';
 
 @Controller('courses')
 export class CoursesController {
+  constructor(private readonly coursesService: CoursesService) {}
 
   @Get()
   findAll(): string {
---
+++ src/courses/courses.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { threadId } from 'worker_threads';
+import { Course } from './entities/Course.entity';
+
+@Injectable()
+export class CoursesService {
+  private courses: Course[] = [
+    {
+      id: 1,
+      name: "Fundamento nestjs",
+      description: "Fundamento nestj",
+      tags: ["node", "nestjs", "typescript"]
+    },
+    {
+      id: 2,
+      name: "Fundamento typescript",
+      description: "Fundamento type",
+      tags: ["node", "typescript"]
+    }
+  ];
+
+  findAll() {
+    return this.courses;
+  }
+
+  findOneById(id: string) {
+    return this.courses.find((course) => course.id === Number(id))
+  }
+
+  create(createCourseDto: any) {
+    return this.courses.push(createCourseDto)
+  }
+
+  update(id: string, updateCourseDto: any) {
+    const indexCourse = this.courses.findIndex(course => course.id === Number(id))
+
+    return this.courses[indexCourse] = updateCourseDto;
+  }
+
+  delete(id: string) {
+    const indexCourse = this.courses.findIndex(course => course.id === Number(id))
+
+    if (indexCourse >= 0) {
+      this.courses.splice(indexCourse, 1)
+    }
+  }
+}
