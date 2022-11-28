e37f73788a1e44a65584e516dec740b8e4877fcb
Vinicius <viniholtman28@gmail.com>
2022-11-28 09:40:33 -0300

    Implemented class validator

--- src/courses/dto/update-course.dto.ts
+++ src/courses/dto/update-course.dto.ts
@@ -1,5 +1,5 @@
-export class UpdateCourseDto {
-  name?: string;
-  description?: string;
-  tags?: string[];
-}
+import { CreateCourseDto } from "./create-course.dto";
+import { PartialType } from 
+
+
+export class UpdateCourseDto extends PartialType(CreateCourseDto) {}
